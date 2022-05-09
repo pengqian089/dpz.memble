5439116f4b61a16e45f67d7795a9932a83ecc7bf
pengqian089 <pengqian089@gmail.com>
2022-05-10 02:49:01 +0800

    完成录音功能

--- dpz.memble.mobile/src/common.js
+++ dpz.memble.mobile/src/common.js
@@ -77,3 +77,137 @@ export async function handleUploadResponse(response, callback = null) {
         return Promise.reject(e.toString());
     }
 }
+
+let audioChunks = [],
+    mediaRecorder = null,
+    audio = null;
+
+export const record = {
+    /**
+     * 播放录音音频
+     * @param {function} playAction 播放录音音频后回调函数
+     * @param {function} endAction 播放录音音频完毕后回调函数
+     * */
+    async playAudio(playAction = null, endAction = null) {
+        if (audioChunks.length === 0) return;
+        if(audio === null) {
+            const audioBlob = new Blob(audioChunks);
+            const audioUrl = URL.createObjectURL(audioBlob);
+            audio = new Audio(audioUrl);
+            audio.addEventListener("play", function () {
+                if (typeof (playAction) === "function") {
+                    playAction();
+                }
+            });
+            audio.addEventListener("ended", function () {
+                if (typeof (endAction) === "function") {
+                    endAction();
+                }
+            });
+        }
+        await audio.play();
+    },
+    /**
+     * 暂停录音音频
+     * @param {function} pauseAction 暂停播放音频回调函数
+     * */
+    async pauseAudio(pauseAction = null){
+        if(audio === null){
+            return;
+        }
+        await audio.pause();
+        if (typeof (pauseAction) === "function") {
+            pauseAction();
+        }
+    },
+    /**
+     * 开始录音
+     * @param {function} startAction 开始录音回调函数
+     * */
+    async startRecord(startAction = null) {
+        try {
+            let stream = await navigator.mediaDevices.getUserMedia({audio: true});
+            mediaRecorder = new MediaRecorder(stream);
+            mediaRecorder.addEventListener("dataavailable", event => {
+                audioChunks.push(event.data);
+            });
+            mediaRecorder.addEventListener("stop",() =>{
+                stream.getTracks().forEach(track => {
+                    console.log(track);
+                    track.stop();
+                });
+            });
+            mediaRecorder.start();
+            if (typeof (startAction) === "function") {
+                startAction();
+            }
+        } catch (e) {
+            warning("已拒绝授权麦克风权限，无法使用录音");
+        }
+    },
+    /**
+     * 结束录音
+     * @param {function} endAction 结束录音回调函数
+     * */
+    endRecord(endAction = null) {
+        if (mediaRecorder === null) return;
+        mediaRecorder.stop();
+        if (typeof (endAction) === "function") {
+            endAction();
+        }
+    },
+    /**
+     * 清除录音
+     * @param {function} clearAction 清除录音回调函数
+     * */
+    clearAudio(clearAction = null) {
+        audioChunks = [];
+        audio = null;
+        if (typeof (clearAction) === "function") {
+            clearAction();
+        }
+    },
+    /**
+     * 上传录音
+     * @param {function} uploadComplete 录音上传完成回调函数
+     * @return {string} 音频地址
+     * */
+    async uploadAudio(uploadComplete = null) {
+        if (audioChunks.length === 0) return Promise.reject("没有获取到音频");
+        const audioBlob = new Blob(audioChunks);
+        let form = new FormData();
+        form.append("record", audioBlob, `${new Date().getTime()}.wav`);
+        let response = await fetch("/Audio/Upload", {
+            method: "post",
+            body: form
+        });
+
+        let result = await handleResponse(response);
+        if (typeof (uploadComplete) === "function") {
+            uploadComplete();
+        }
+        return Promise.resolve(`/Audio/Result/${result.id}`);
+
+        // if(response.ok) {
+        //     let result = await response.json();
+        //     if (result.success) {
+        //         if (typeof (uploadComplete) === "function") {
+        //             uploadComplete();
+        //         }
+        //         return Promise.resolve(`/Audio/Result/${result.data.id}`);
+        //     }else{
+        //         warning(result.msg);
+        //         return Promise.reject(result.msg);
+        //     }
+        // }else {
+        //     warning(response.statusText);
+        //     return Promise.reject(response.statusText);
+        // }
+    },
+    saveAs(){
+        if (audioChunks.length === 0) return Promise.reject("没有获取到音频");
+        const audioBlob = new Blob(audioChunks);
+        let url  = URL.createObjectURL(audioBlob);
+        window.open(url);
+    }
+};
--- dpz.memble.mobile/src/views/PublishMumbleView.vue
+++ dpz.memble.mobile/src/views/PublishMumbleView.vue
@@ -1,6 +1,17 @@
 <template>
   <van-nav-bar title="发布碎碎念" left-arrow left-text="返回" @click="back"/>
   <div style="padding:0.5em">
+    <div style="padding: 1em">
+      <van-button :disabled="btnStartRecord" type="primary" @click="onRecord">
+        {{btnStartRecordText}}
+      </van-button>
+      <van-button :disabled="btnPlay" type="primary" @click="onTogglePlay">
+        {{btnPlayText}}
+      </van-button>
+      <van-button :disabled="btnClear" @click="onClear" type="primary">清除录音</van-button>
+      <van-button :disabled="btnUpload" @click="onUploadAudio" type="primary">上传录音</van-button>
+      <van-button @click="save" type="primary">保存录音</van-button>
+    </div>
     <van-form @submit="publish" class="content">
       <van-cell-group inset>
         <md-editor
@@ -26,7 +37,7 @@
 import MdEditor from "md-editor-v3";
 import "md-editor-v3/lib/style.css";
 import _ from "lodash";
-import {success, warning, handleUploadResponse} from "@/common";
+import {success, warning, handleUploadResponse, record} from "@/common";
 
 export default {
   name: "PublishMumbleView",
@@ -42,26 +53,27 @@ export default {
         'image',
         'strikeThrough',
         '-',
-        'quote',
-        'unorderedList',
-        'orderedList',
-        '-',
-        'codeRow',
-        'code',
         'link',
         'table',
         '=',
         'prettier',
-        'pageFullscreen',
-        'preview',
-        'catalog',
+        'pageFullscreen'
       ],
+      btnStartRecord: false,
+      btnPlay: true,
+      btnClear: true,
+      btnUpload: true,
+      btnStartRecordText:"开始录音",
+      btnPlayText: "试听"
     }
   },
   components: {
     MdEditor
   },
   methods: {
+    /**
+     * 上传图片
+     * */
     async uploadImage(files, callback) {
       if (files.length <= 0) {
         warning("请选择图片");
@@ -84,9 +96,15 @@ export default {
       let url = await handleUploadResponse(response);
       callback([url]);
     },
+    /**
+     * 获取html内容
+     * */
     onHtmlChanged(html) {
       this.mumble.htmlContent = html;
     },
+    /**
+     * 发布碎碎念
+     * */
     async publish() {
       this.publishing = false;
       if (_.isEmpty(this.mumble.markdown)) {
@@ -111,8 +129,79 @@ export default {
       this.publishing = false;
       await this.$router.push({name: "mumble"});
     },
-    back(){
+    /**
+     * 返回
+     * */
+    back() {
       history.back();
+    },
+    /**
+     * 开始录音
+     * */
+    async onRecord() {
+      let that = this;
+      if(this.btnStartRecordText === "开始录音"){
+        await record.startRecord(() => {
+          that.btnStartRecordText = "结束录音";
+        });
+      }else{
+        record.endRecord(() => {
+          that.btnStartRecord = true;
+          that.btnPlay = false;
+          that.btnClear = false;
+          that.btnUpload = false;
+          that.btnStartRecordText = "开始录音";
+        });
+      }
+
+    },
+    onTogglePlay() {
+      let that = this;
+      if(this.btnPlayText === "试听" || this.btnPlayText === "继续播放"){
+        record.playAudio(() =>{
+          that.btnPlayText = "暂停";
+          that.btnStartRecord = true;
+          that.btnPlay = false;
+          that.btnClear = true;
+          that.btnUpload = true;
+        },() =>{
+          that.btnPlayText = "试听";
+          that.btnStartRecord = true;
+          that.btnPlay = false;
+          that.btnClear = false;
+          that.btnUpload = false;
+        });
+      }else{
+        record.pauseAudio(() =>{
+          that.btnPlayText = "继续播放";
+          that.btnStartRecord = true;
+          that.btnPlay = false;
+          that.btnClear = true;
+          that.btnUpload = true;
+        });
+      }
+    },
+    onClear(){
+      let that = this;
+      record.clearAudio(() =>{
+        that.btnStartRecord = false;
+        that.btnPlay = true;
+        that.btnClear = true;
+        that.btnUpload = true;
+      });
+    },
+    async onUploadAudio(){
+      let that = this;
+      let result = await record.uploadAudio(() => {
+        that.btnStartRecord = false;
+        that.btnPlay = true;
+        that.btnClear = true;
+        that.btnUpload = true;
+      });
+      console.log(result);
+    },
+    save(){
+      record.saveAs();
     }
   }
 }
